de9e696fd69876f2f096be8a94e1c2abf4c8cf77
Mikko Berggren Ettienne <mikkobe@gmail.com>
2013-10-25 20:54:34 +0200

    add config file

---
+++ config.py
@@ -0,0 +1,18 @@
+import logging
+import os.path
+logging.basicConfig(level=logging.WARNING, filename='import.log', filemode='w')
+#logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.WARNING)
+
+
+path = 'text_values'
+deptor_file = 'debitor.txt'
+creditor_file = 'kreditor.txt'
+item_file = 'vare.txt'
+sales_invoice_line = 'salgsfakturalinie.txt'
+sales_invoice_head = 'salgsfakturahovede.txt'
+uri = "mongodb://localhost/invoice"
+n_threads = 4
+
+home_dir = os.path.expanduser("~")
+home_config = os.path.join(home_dir, ".trade_tools.config.py")
